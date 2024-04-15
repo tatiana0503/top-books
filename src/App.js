6098f99297f5588fd53d26dd8185a4c519b0e9f2
tatiana0503 <tzhamoitina@gmail.com>
2024-04-15 14:19:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState(data);
+  const [showText, setShowText] = useState(false);
+  const removeBook = (id) => {
+    let newBooks = books.filter(book => book.id !== id);
+    setBooks(newBooks);
+
+  }
+  const showTextClick = (item) => {
+       item.showMore = !item.showMore;
+       setShowText(!showText)  
+      }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <div className='container'>
+        <h1>TOP {books.length} BOOKS TO READ IN  2023</h1>
+      </div>
+
+      {books.map((item) => {
+        const {id, name, author, description, image, showMore} = item;
+       
+      
+        return (
+          <div key={id}>
+            <div className='container'>
+              <h2>{id} - {name}</h2>
+            </div>
+            <div className='container'>
+              <h3><i>by {author}</i></h3>
+            </div>
+            <div className='container'>
+              <img src={image} width="300px" alt="book"/>
+            </div>
+            <div className='container'>
+              <p>{showMore ? description : description.substring(0,300) + "..."}
+              <button className='btn' onClick = {() => showTextClick(item)}>{showMore ? "Show less" : "Show more"}</button></p>
+            </div>
+            <div className='container'>
+              <button onClick={() => removeBook(id)}>REMOVE</button>
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        id: 1,
+        name: 'I Love Russia',
+        author: 'Elena Kostyuchenko',
+        description: 'In 2005, when Kostyuchenko started as an intern at the storied Russian independent newspaper Novaya Gazeta, Vladimir Putin was relatively new to the Presidency and high oil prices were fuelling a consumer boom. But Kostyuchenko was less interested in the Russia hurtling forward than the one left behind, a place—or, rather, a people—defined by trauma and disorientation, hardiness and resolve. A decade and a half later, she spent two weeks living inside a state-run residential facility for people with psychiatric and neurological disorders. The article that emerged from that experience—a wrenching and visceral text whose details almost seem to waft off the page—is the masterwork at the heart of “I Love Russia,” a memoir and collection of reportage translated by Bela Shayevich and Ilona Yazhbin Chavasse. In February of 2022, Kostyuchenko crossed into Ukraine, becoming one of an exceedingly small number of Russian journalists who managed to report from the war zone. She filed dispatches on Russia’s occupation and bombardment of Ukraine’s southern cities, bracing accounts laced with a sense of guilt and the utter futility of that guilt. But it was only upon leaving Ukraine that she fell victim to what may have been an act of Russian aggression: a suspected poisoning attempt inside Germany. Kostyuchenko’s writings are also a personal reckoning, an attempt to work through how she missed—or, rather, failed to adequately react to—Russia’s descent into fascism.',
+        image: 'https://m.media-amazon.com/images/I/91Oo8x04aoL._SL1500_.jpg',
+        showMore: false
+    },
+
+    {
+        id: 2,
+        name: 'A Day in the Life of Abed Salama',
+        author: 'Nathan Thrall',
+        description: 'In 2012, a catastrophic traffic collision in Jerusalem left a school bus filled with Palestinian children on fire for more than thirty minutes before emergency workers arrived. In this chronicle of the disaster, Thrall, a Jerusalem-based journalist, follows the father of one of the victims, and examines the response to the crash within the context of modern Palestinian dispossession. He depicts Israel’s “architecture of segregation”—encompassing checkpoints and byzantine transit rules—which needlessly complicated the rescue, leading to a delay that left “small, scorched backpacks” on the asphalt. Thrall’s account is a powerful evocation of a two-tiered society that treats children as potential combatants.',
+        image: 'https://m.media-amazon.com/images/I/41HFP1AkfrL._SY445_SX342_.jpg',
+        showMore: false
+    },
+
+    {
+        id: 3,
+        name: 'The Country of the Blind',
+        author: 'Andrew Leland ',
+        description: 'In this moving memoir, Andrew Leland recounts his journey from sight to blindness, tracing his ever-shifting relationship to his diminishing vision. Suspended between the worlds of blindness and sight—he will soon lose his vision entirely—Leland explores the history and culture of blindness: its intersections with medicine, technology, ableism. He travels to a residential school for the blind, where he dons shades that block his vision, and learns to cook meals and cross streets. One former student tells him, “Until you get profoundly lost, and know it’s within you to get unlost, you’re not trained—until you know it’s not an emergency but a magnificent puzzle.”',
+        image: 'https://m.media-amazon.com/images/I/51OaroA5O4L.jpg',
+        showMore: false
+    },
+
+    {
+        id: 4,
+        name: 'The Bee Sting',
+        author: 'Paul Murray',
+        description: 'Paul Murray’s fourth novel is about the eeriness of transformative change. Its more than six hundred pages employ a rotating structure: the four members of the Barnes family—twelve-year-old PJ, his sister Cass, and his parents Imelda and Dickie—take turns as narrator. Irony, both caustic and elegiac, flourishes in the knowledge gaps between characters. Again and again, details come back reframed or reanimated by another perspective. It’s hard to resist Murray in his schoolyard mode, wittily choreographing nerds and bullies. The chapters featuring Imelda and Dickie are thornier, more treacherous, and formally more ambitious, using stream of consciousness to invoke the shattering power of grief and lust. Murray is interested in denial and how it ultimately fails to contain our unruly attachments and weird desperation. The catastrophic price of such denial is evident in the book’s frequent allusions to the climate crisis. As the book continues, the Earth’s climate and the apocalyptic climate of the Barnes family appear almost to merge, and what began as a coming-of-age saga pulls in stranger and darker forces.',
+        image: 'https://upload.wikimedia.org/wikipedia/en/c/ce/The_Bee_Sting.jpg',
+        showMore: false
+    },
+
+    {
+        id: 5,
+        name: 'Fear Is Just a Word',
+        author: 'Azam Ahmed',
+        description: 'In 2010, the Zeta drug cartel seized control of San Fernando, Mexico, ushering in a wave of kidnappings and murders. Its tactics were brutal: it forced its hostages to fight one another, and sometimes dissolved its victims’ bodies in acid. Ahmed, a correspondent for the Times, retraces the story of Miriam Rodríguez, whose daughter was abducted, in 2014, and later killed. After government officials proved ineffectual, Rodríguez embarked on a search for justice, eventually uncovering the identities of several people complicit in the murder. Tragically, Rodríguez herself failed to escape the violence: she was shot to death for challenging “the primacy of organized crime.”',
+        image: 'https://m.media-amazon.com/images/I/41PNFPzulNL._SY445_SX342_.jpg',
+        showMore: false
+    }
+]
